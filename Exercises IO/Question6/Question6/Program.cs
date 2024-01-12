0c9ad797bbe27f5ad588e92c05eed6486e89c9f8
Yuri Severo Pinto Costa <127903285+Yuri-Severo@users.noreply.github.com>
2024-01-12 19:11:19 -0300

    final file update

---
+++ Exercises IO/Question6/Question6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the values in the format: A B C (with spaces between)");
+            string[] input = Console.ReadLine().Split();
+
+            double a = double.Parse(input[0],CultureInfo.InvariantCulture);
+            double b = double.Parse(input[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(input[2], CultureInfo.InvariantCulture);
+
+            double triangle = (a * c) / 2;
+            double circle = c * 3.14159;
+            double trapeze = ((a + b) * c) / 2;
+            double square = Math.Pow(b, 2);
+            double rectangle = a * b;
+
+            Console.WriteLine("triangle: "+triangle.ToString("F3",CultureInfo.InvariantCulture));
+            Console.WriteLine("circle: " + circle.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("trapeze: " + trapeze.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("square: " + square.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("rectangle: " + rectangle.ToString("F3", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
