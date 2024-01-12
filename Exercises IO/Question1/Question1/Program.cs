1296d29cb92a9bdda91cff3f0d834f699d7952b7
Yuri Severo Pinto Costa <127903285+Yuri-Severo@users.noreply.github.com>
2024-01-12 13:21:11 -0300

    Exercises IO
    I have started a new list of exercises from the course, they are focused in Input, processing and output. I already had done these kind of exercise but never in C#

---
+++ Exercises IO/Question1/Question1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Make a program that reads the input of two integers values, makes 
+ * the sum of them and show the result on the  the screen */
+
+using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the first number:");
+            int val1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second number:");
+            int val2 = int.Parse(Console.ReadLine());
+            int soma = val1 + val2;
+            Console.WriteLine("SOMA = " + soma);
+
+        }
+    }
+}
---
+++ Exercises IO/Question2/Question2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Make a program to read the value of the radius of a circle, and then display the value of 
+ * the area of this circle to four decimal places */
+
+using System;
+using System.Globalization;
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the value of the radius of the circle:");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = 3.14159 * Math.Pow(radius,2);
+
+            Console.WriteLine("Area = "+area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+};
